1b15b59a1ce854c7d4a974753c7535ea49ee495e
TonyWu20 <tony.w21@gmail.com>
2025-03-05 10:45:17 +0800

    use fzf_tmux

--- fish/default.nix
+++ fish/default.nix
@@ -9,6 +9,7 @@
     set -gx FZF_PREVIEW_FILE_CMD 'bat --style=header,numbers,grid --line-range :300 --color=always {}'
     set -gx FZF_PREVIEW_DIR_CMD 'eza -l --git --no-permissions --icons --no-user --level=2 -T '
     set -gx FZF_CTRL_T_OPTS '--walker-skip .git,node_modules,target --preview \"bat -n --color=always {}\" --bind \"ctrl-/:change-preview-window(down|hidden|)\"'
+    set -U FZF_TMUX 1
     ";
     shellAbbrs = {
       vim = "nvim";
