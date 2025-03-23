e0ce1f93461620d5c5eef59cb87cfa1eb6482ce9
TonyWu20 <tony.w21@gmail.com>
2025-03-23 23:59:52 +0800

    Solved cpu module display in `catppuccin-tmux`

--- tmux/default.nix
+++ tmux/default.nix
@@ -4,7 +4,10 @@
     enable = true;
     shell = "${pkgs.fish}/bin/fish";
     keyMode = "vi";
-    extraConfig = builtins.readFile ./tmux.conf;
+    extraConfig = builtins.concatStringsSep "\n" [
+      (builtins.readFile ./tmux.conf)
+      "run-shell ${pkgs.tmuxPlugins.cpu}/share/tmux-plugins/cpu/cpu.tmux"
+    ];
     terminal = "xterm-256colors";
     plugins = with pkgs; [
       tmuxPlugins.resurrect
@@ -12,9 +15,9 @@
       tmuxPlugins.mode-indicator
       tmuxPlugins.yank
       tmuxPlugins.sensible
+      tmuxPlugins.catppuccin
       tmuxPlugins.cpu
       # tmux-mem-cpu-load
-      tmuxPlugins.catppuccin
     ];
   };
 }
