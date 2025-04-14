43d3e928ea237f3238298c35c177e21413c6861a
TonyWu20 <tony.w21@gmail.com>
2025-04-15 05:34:46 +0800

    Downgrade `wezterm` to `24.11` branch of `nixpkgs`

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     # NixOS official package source, using the nixos-24.11 branch here
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
     nvimdots = { url = "github:TonyWu20/nvimdots/nix"; inputs.nixpkgs.follows = "nixpkgs"; };
     catppuccin.url = "github:catppuccin/nix";
     fenix = { url = "github:nix-community/fenix"; inputs.nixpkgs.follows = "nixpkgs"; };
@@ -19,15 +20,34 @@
     castep.url = "git+file:///home/tony/Downloads/CASTEP-25.12-nixos";
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, fenix, catppuccin, castep, ... }: {
+  outputs = inputs@{ self, nixpkgs, nixpkgs-stable, home-manager, fenix, catppuccin, castep, ... }: {
     packages.x86_64-linux.default = fenix.packages.x86_64-linux.stable.toolchain;
     nixosConfigurations = {
       # Please replace my-nixos with your hostname
-      nixos = nixpkgs.lib.nixosSystem {
+      nixos = nixpkgs.lib.nixosSystem rec {
         system = "x86_64-linux";
+        # The `specialArgs` parameter passes the
+        # non-default nixpkgs instances to other nix modules
+        specialArgs = {
+          # To use packages from nixpkgs-stable,
+          # we configure some parameters for it first
+          pkgs-stable = import nixpkgs-stable {
+            # Refer to the `system` parameter from
+            # the outer scope recursively
+            inherit system;
+            config.allowUnfree = true;
+          };
+        };
         modules = [
-          ({ pkgs, ... }: {
-            nixpkgs.overlays = [ fenix.overlays.default castep.overlays.default ];
+          ({ pkgs, pkgs-stable, ... }: {
+
+            nixpkgs.overlays = [
+              fenix.overlays.default
+              castep.overlays.default
+              (self: super: {
+                wezterm = pkgs-stable.wezterm;
+              })
+            ];
             environment.systemPackages = with pkgs; [
               (fenix.packages.x86_64-linux.stable.withComponents [
                 "cargo"
--- home.nix
+++ home.nix
@@ -1,7 +1,4 @@
-{ self, inputs, config, pkgs, catppuccin, ... }:
-let
-  inherit (inputs);
-in
+{ pkgs, ... }:
 {
   imports = [
     ./starship.nix
@@ -165,6 +162,7 @@ in
     dunst
 
     # texlive later
+    alacritty
   ];
 
   # basic configuration of git, please change to your own
@@ -198,6 +196,12 @@ in
       host = "mac";
       user = "tonywu";
       hostname = "10.147.17.25";
+      identityFile = "~/.ssh/id_ed25519";
+    };
+    matchBlocks.local-mac = {
+      host = "local-mac";
+      user = "tonywu";
+      hostname = "10.0.0.1";
       identityFile = "~/.ssh/id_ed25519";
     };
     matchBlocks.font = {
