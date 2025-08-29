70923cbdc1d2143deeca8175457b5369b8e0cc2d
TonyWu20 <tony.w21@gmail.com>
2025-08-29 17:49:03 +0800

    prettier formatted

--- flake.nix
+++ flake.nix
@@ -26,83 +26,109 @@
     };
   };
 
-  outputs = inputs@{ self, nvimdots, nixpkgs, nixpkgs-stable, home-manager, fenix, catppuccin, castep, castep_devShells, sops-nix, ... }: {
-    packages.x86_64-linux.default = fenix.packages.x86_64-linux.stable.toolchain;
-    devShells = castep_devShells.devShells;
-    nixosConfigurations = {
-      # Please replace my-nixos with your hostname
-      nixos = nixpkgs.lib.nixosSystem rec {
-        system = "x86_64-linux";
-        # The `specialArgs` parameter passes the
-        # non-default nixpkgs instances to other nix modules
-        specialArgs = {
-          # To use packages from nixpkgs-stable,
-          # we configure some parameters for it first
-          pkgs-stable = import nixpkgs-stable {
-            # Refer to the `system` parameter from
-            # the outer scope recursively
-            inherit system;
-            config.allowUnfree = true;
-          };
+  outputs = inputs@{ self, nvimdots, nixpkgs, nixpkgs-stable, home-manager, fenix, catppuccin, castep, castep_devShells, sops-nix, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system; config = {
+        allowUnfree = true;
+        cudaSupport = true;
+      };
+      };
+    in
+    {
+      packages.x86_64-linux.default = fenix.packages.x86_64-linux.stable.toolchain;
+      devShells.${system} = {
+        rs_font = pkgs.mkShell {
+          packages = with pkgs; [
+            stdenv
+            fish
+          ];
+          buildInputs = with pkgs; [
+            fontconfig
+          ];
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+          shellHook = ''
+            exec fish
+          '';
         };
-        modules = [
-          ({ pkgs, pkgs-stable, ... }: {
-
-            nixpkgs.overlays = [
-              fenix.overlays.default
-              castep.overlays.default
-              (self: super: {
-                # wezterm = pkgs-stable.wezterm;
-                # hyprland = pkgs-stable.hyprland;
-              })
-            ];
-            nix.registry = {
-              myShell.flake = self;
+      };
+      nixosConfigurations = {
+        # Please replace my-nixos with your hostname
+        nixos = nixpkgs.lib.nixosSystem rec {
+          system = "x86_64-linux";
+          # The `specialArgs` parameter passes the
+          # non-default nixpkgs instances to other nix modules
+          specialArgs = {
+            # To use packages from nixpkgs-stable,
+            # we configure some parameters for it first
+            pkgs-stable = import nixpkgs-stable {
+              # Refer to the `system` parameter from
+              # the outer scope recursively
+              inherit system;
+              config.allowUnfree = true;
             };
-            environment.systemPackages = with pkgs; [
-              (fenix.packages.x86_64-linux.stable.withComponents [
-                "cargo"
-                "clippy"
-                "rust-src"
-                "rustc"
-                "rustfmt"
-                "rust-analyzer"
-              ])
-              gcc
-            ];
-          })
-          sops-nix.nixosModules.sops
-          # Import the previous configuration.nix we used,
-          # so the old configuration file still takes effect
-          ./configuration.nix
-          ./fcitx5
-          # catppuccin/nix
-          catppuccin.nixosModules.catppuccin
-          # make home-manager as a module of nixos
-          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              sharedModules = [
-                nvimdots.homeManagerModules.default
-                catppuccin.homeModules.catppuccin
+          };
+          modules = [
+            ({ pkgs, pkgs-stable, ... }: {
+
+              nixpkgs.overlays = [
+                fenix.overlays.default
+                castep.overlays.default
+                (self: super: {
+                  # wezterm = pkgs-stable.wezterm;
+                  # hyprland = pkgs-stable.hyprland;
+                })
               ];
-              users.tony = {
-                imports = [
-                  ./home.nix
-                  ./nvim/nvimdots.nix
-                  # catppuccin.homeManagerModules.catppuccin
-                ];
+              nix.registry = {
+                myShell.flake = self;
+                castep.flake = castep_devShells;
               };
-              backupFileExtension = "backup";
-              extraSpecialArgs = { inherit inputs; };
-            };
+              environment.systemPackages = with pkgs; [
+                (fenix.packages.x86_64-linux.stable.withComponents [
+                  "cargo"
+                  "clippy"
+                  "rust-src"
+                  "rustc"
+                  "rustfmt"
+                  "rust-analyzer"
+                ])
+                gcc
+              ];
+            })
+            sops-nix.nixosModules.sops
+            # Import the previous configuration.nix we used,
+            # so the old configuration file still takes effect
+            ./configuration.nix
+            ./fcitx5
+            # catppuccin/nix
+            catppuccin.nixosModules.catppuccin
+            # make home-manager as a module of nixos
+            # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                sharedModules = [
+                  nvimdots.homeManagerModules.default
+                  catppuccin.homeModules.catppuccin
+                ];
+                users.tony = {
+                  imports = [
+                    ./home.nix
+                    # catppuccin.homeManagerModules.catppuccin
+                  ];
+                };
+                backupFileExtension = "backup";
+                extraSpecialArgs = { inherit inputs; };
+              };
 
-          }
-        ];
+            }
+          ];
+        };
       };
     };
-  };
 }
