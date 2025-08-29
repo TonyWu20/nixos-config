12b49051e38d0efcd304378f57e6d3ab06dd3fcc
TonyWu20 <tony.w21@gmail.com>
2025-08-29 17:49:47 +0800

    fit new display

--- hypr/display.nix
+++ hypr/display.nix
@@ -1,7 +1,7 @@
 { config, ... }: {
   wayland.windowManager.hyprland.settings = {
     monitor = [
-      "HDMI-A-1, 3840x2160, auto, 2"
+      "HDMI-A-1, 2560x1440@100, auto, 2"
       "HDMI-A-2, 1920x1080@100, auto, auto"
     ];
     workspace = [
