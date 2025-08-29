eeff9a16ffe89dda8572485b74718b0c88f0c5c0
TonyWu20 <tony.w21@gmail.com>
2025-08-29 17:48:14 +0800

    adjust `nfs` export

--- configuration.nix
+++ configuration.nix
@@ -235,10 +235,17 @@
     "/export/CASTEP-6.11-nixos" = {
       device = "/home/tony/Downloads/CASTEP-6.11-nixos";
       options = [ "bind" ];
+      fsType = "nfs";
+    };
+    "/export/CASTEP-25.12-nixos" = {
+      device = "/home/tony/Downloads/CASTEP-25.12-nixos";
+      options = [ "bind" ];
+      fsType = "nfs";
     };
     "/export/castep_devshell" = {
       device = "/home/tony/Downloads/castep_devshell";
       options = [ "bind" ];
+      fsType = "nfs";
     };
   };
   boot.supportedFilesystems = [ "nfs" ];
@@ -247,9 +254,9 @@
     enable = true;
     exports = ''
       /export       10.0.0.2(rw,fsid=0,no_subtree_check) 10.0.0.3(rw,fsid=0,no_subtree_check)
-      /export/castep_jobs  10.0.0.2(rw,nohide,insecure,no_subtree_check) 10.0.0.3(rw,nohide,insecure,no_subtree_check)
-      /export/CASTEP-6.11-nixos 10.0.0.2(rw,nohide,insecure,no_subtree_check) 10.0.0.3(rw,nohide,insecure,no_subtree_check)
-      /export/castep_devshell 10.0.0.2(rw,nohide,insecure,no_subtree_check) 10.0.0.3(rw,nohide,insecure,no_subtree_check)
+      /export/castep_jobs     *(rw,nohide,insecure,no_subtree_check)
+      /export/CASTEP-25.12-nixos     *(rw,nohide,insecure,no_subtree_check)
+      /export/castep_devshell     *(rw,nohide,insecure,no_subtree_check)
     '';
     # fixed rpc.statd port; for firewall
     lockdPort = 4001;
