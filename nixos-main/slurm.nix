f4046614761f4e22e071eab126bf3db9c6ab5ead
TonyWu20 <tony.w21@gmail.com>
2025-09-03 17:52:22 +0800

    separate slurm roles

--- nixos-main/slurm.nix
+++ nixos-main/slurm.nix
@@ -1,5 +1,25 @@
-{ ... }: {
+{ pkgs, ... }: {
   services.slurm = {
     server.enable = true;
   };
+  services.mysql = {
+    enable = true;
+    package = pkgs.mariadb;
+
+    ensureDatabases = [ "slurm_acct_db" ];
+    ensureUsers = [{
+      ensurePermissions = { "slurm_acct_db.*" = "ALL PRIVILEGES"; };
+      name = "slurm";
+    }];
+
+    settings.mysqld = {
+      bind-address = "localhost";
+
+      # recommendations from: https://slurm.schedmd.com/accounting.html#mysql-configuration
+      innodb_buffer_pool_size = "1024M";
+      innodb_log_file_size = "64M";
+      innodb_lock_wait_timeout = 900;
+    };
+  };
+
 }
--- slurm/default.nix
+++ slurm/default.nix
@@ -41,23 +41,4 @@ in
   networking.firewall.trustedInterfaces = [ "enp6s0" ];
 
   environment.variables.SLURM_CONF = builtins.concatStringsSep "/" [ config.services.slurm.etcSlurm "slurm.conf" ];
-  services.mysql = {
-    enable = true;
-    package = pkgs.mariadb;
-
-    ensureDatabases = [ "slurm_acct_db" ];
-    ensureUsers = [{
-      ensurePermissions = { "slurm_acct_db.*" = "ALL PRIVILEGES"; };
-      name = "slurm";
-    }];
-
-    settings.mysqld = {
-      bind-address = "localhost";
-
-      # recommendations from: https://slurm.schedmd.com/accounting.html#mysql-configuration
-      innodb_buffer_pool_size = "1024M";
-      innodb_log_file_size = "64M";
-      innodb_lock_wait_timeout = 900;
-    };
-  };
 }
