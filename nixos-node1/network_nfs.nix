f3a95a05ad37c2c242c5747fb16573043ff4474b
TonyWu20 <tony.w21@gmail.com>
2025-09-03 17:26:35 +0800

    adjust order of host

--- nixos-node1/network_nfs.nix
+++ nixos-node1/network_nfs.nix
@@ -1,13 +1,13 @@
 { ... }: {
   # Open ports in the firewall.
-  services.zerotierone.joinNetworks = [ "b15644912e4d3047" "e3918db483c6bfed"];
+  services.zerotierone.joinNetworks = [ "b15644912e4d3047" "e3918db483c6bfed" ];
   networking.interfaces.enp6s0.ipv4.addresses = [{
     address = "10.0.0.3";
     prefixLength = 24;
   }];
   networking.extraHosts = ''
-    10.0.0.2 nixos
     10.0.0.3 nixos-2
+    10.0.0.2 nixos
   '';
   fileSystems = {
     "/export/castep_jobs" = {
