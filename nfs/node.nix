780cfae28c6589ff1d61b175fbab529fde214d4a
TonyWu20 <tony.w21@gmail.com>
2026-09-10 02:13:51 +0800

    Add nofail to NFS mount options so failed mounts do not block switch

--- nfs/node.nix
+++ nfs/node.nix
@@ -1,41 +1,49 @@
 { ... }: {
   fileSystems = {
     "/export" = {
-      device = "10.0.0.2:/";
+      device = "10.0.0.6:/";
       fsType = "nfs";
+      options = [ "nofail" ];
     };
     "/export/castep_jobs" = {
-      device = "10.0.0.2:/castep_jobs";
+      device = "10.0.0.6:/castep_jobs";
       fsType = "nfs";
+      options = [ "nofail" ];
     };
     "/export/g16" = {
-      device = "10.0.0.2:/g16";
+      device = "10.0.0.6:/g16";
       fsType = "nfs";
+      options = [ "nofail" ];
     };
     "/export/gauss_shell" = {
-      device = "10.0.0.2:/gauss_shell";
+      device = "10.0.0.6:/gauss_shell";
       fsType = "nfs";
+      options = [ "nofail" ];
     };
     "/export/gaussian_jobs" = {
-      device = "10.0.0.2:/gaussian_jobs";
+      device = "10.0.0.6:/gaussian_jobs";
       fsType = "nfs";
+      options = [ "nofail" ];
     };
     "/export/Potentials" = {
-      device = "10.0.0.2:/Potentials";
+      device = "10.0.0.6:/Potentials";
       fsType = "nfs";
+      options = [ "nofail" ];
     };
     "/export/public_castep_jobs" = {
-      device = "10.0.0.2:/public_castep_jobs";
+      device = "10.0.0.6:/public_castep_jobs";
       fsType = "nfs";
+      options = [ "nofail" ];
     };
     "/export/lammps_jobs" = {
-      device = "10.0.0.2:/lammps_jobs";
+      device = "10.0.0.6:/lammps_jobs";
       fsType = "nfs";
+      options = [ "nofail" ];
     };
     "/export/castep-rust-eigensolve" = {
-      device = "10.0.0.2:/castep-rust-eigensolve";
+      device = "10.0.0.6:/castep-rust-eigensolve";
       fsType = "nfs";
-      options = [ "rw" "noauto" "x-systemd.automount" ];
+      options = [ "rw" "noauto" "x-systemd.automount" "nofail" ];
     };
   };
 }
